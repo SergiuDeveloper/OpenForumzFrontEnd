4852bb73f3399569dbd7640fb41260a6649a743a
SergiuDeveloper <sergiu.nistor@info.uaic.ro>
2023-11-17 14:17:25 +0200

    Fix

--- src/auth/auth.tsx
+++ src/auth/auth.tsx
@@ -8,7 +8,7 @@ const cookies = new Cookies(null, { path: '/' });
 const firebaseConfig: FirebaseOptions = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    authDomain: process.env.NODE_ENV == "development" ? "opensourcesocialmedia.firebaseapp.com" : process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
